e05c6a83a722fb64f25b0f8ad4d7dee6166e5c6f
钟国政 <victinzhong@163.com>
2023-10-02 19:48:52 +0800

    feat(main): init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  //这里进行配置别名
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // @代替src
+      "#": path.resolve("./types"), // #代替types
+    },
+  },
+})
